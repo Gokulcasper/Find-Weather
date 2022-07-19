8683de5e1054a5e1bd8f3e9591a65b7a71e633af
Gokulcasper <ronitcasper215@gmail.com>
2022-07-19 20:57:15 +0530

    Feat : Weather

--- script.js
+++ script.js
@@ -24,7 +24,6 @@ let weather = {
     document.querySelector(".humidity").innerText =
       "Humidity: " + humidity + "%";
     document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
-    document.querySelector(".weather").classList.remove("loading");
     document.body.style.backgroundImage =
       "url('https://source.unsplash.com/1600x900/?" + name + "')";
   },
